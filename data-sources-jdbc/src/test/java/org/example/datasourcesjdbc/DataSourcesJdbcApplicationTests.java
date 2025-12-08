d8808beeefe1a0b5839ff3ed47dccf4d2ec635d0
nbeder <norbert.beder@endava.com>
2025-12-08 11:57:42 +0200

    init commit

---
+++ data-sources-jdbc/src/test/java/org/example/datasourcesjdbc/DataSourcesJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datasourcesjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSourcesJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
