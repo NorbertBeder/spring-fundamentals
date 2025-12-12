49b35fa4feacf2086e5c7439898dd0b771c5ce7c
nbeder <norbert.beder@endava.com>
2025-12-12 10:45:11 +0200

    update PostgreSQL insert with flyway migration

---
+++ postgres-sql/src/main/resources/db/migration/V1__create_purchase_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE purchase(
+    id SERIAL PRIMARY KEY,
+    product VARCHAR(50) NOT NULL,
+    price DOUBLE PRECISION NOT NULL
+)
---
+++ postgres-sql/src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO purchase(
+    product,
+    price
+) VALUES
+    ('Laptop', 999.99),
+    ('Smartphone', 499.49),
+    ('Tablet', 299.29
+)
